87f996df6f47b2cf6fa8fdb68cdc08b604e82da9
Ben Boeckel <mathstuf@gmail.com>
2015-09-01 00:27:47 -0400

    interface: add basic interface

--- src/interface.rs
+++ src/interface.rs
@@ -1,1 +1,9 @@
-pub struct DBusInterface; // TODO: Implement.
+extern crate dbus;
+use self::dbus::{Connection, Error, Message, MessageItem};
+
+pub trait DBusInterface {
+    fn call_method(&mut self, method: &str, conn: &Connection, m: &Message) -> Option<Message>;
+
+    fn set_property(&mut self, prop: &str, value: MessageItem) -> Result<(), Error>;
+    fn get_property(&self, prop: &str) -> Result<(), Error>;
+}
