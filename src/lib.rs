73b8bac93e6105b5d742f6709bf1e8953d6bf733
Ben Boeckel <mathstuf@gmail.com>
2015-09-12 14:05:37 -0400

    lib: simplify re-exports

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,8 @@ mod runner;
 mod server;
 mod target;
 
-pub use connection::DBusConnection as DBusConnection;
-pub use error::DBusError as DBusError;
-pub use runner::DBusRunner as DBusRunner;
-pub use server::DBusServer as DBusServer;
-pub use target::DBusTarget as DBusTarget;
+pub use connection::DBusConnection;
+pub use error::DBusError;
+pub use runner::DBusRunner;
+pub use server::DBusServer;
+pub use target::DBusTarget;
