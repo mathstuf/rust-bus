95f00b6c7c0c34439c42eb4baa102620582f96d8
Ben Boeckel <mathstuf@gmail.com>
2015-09-12 13:18:31 -0400

    lib: explicitly export symbols

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
-pub mod connection;
-pub mod error;
-pub mod server;
-pub mod target;
+mod connection;
+mod error;
+mod server;
+mod target;
+
+pub use connection::DBusConnection as DBusConnection;
+pub use error::DBusError as DBusError;
+pub use server::DBusServer as DBusServer;
+pub use target::DBusTarget as DBusTarget;
